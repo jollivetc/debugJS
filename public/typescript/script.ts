a29df0c17f77e48575e2e0a4e797a733853c3f0d
Christophe Jollivet <christophe.jollivet@gmail.com>
2015-11-20 17:05:40 +0100

    first version for typescript

---
+++ public/typescript/script.ts
@@ -0,0 +1,30 @@
+class Greeter {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+     
+    constructor (element: HTMLElement) { 
+        this.element = element;
+        this.element.innerHTML += "Il est : ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerHTML = new Date().toUTCString();
+    }
+ 
+    start() {
+        this.timerToken = setInterval(
+            () => this.span.innerHTML= new Date().toUTCString(), 500
+            );
+    }
+ 
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+ 
+}
+ 
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new Greeter(el);
+    greeter.start();
+};
