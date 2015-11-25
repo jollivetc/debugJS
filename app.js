b0e7a96afaedf0cf3261a500ce93a5f3bfb3767d
Christophe Jollivet <christophe.jollivet@gmail.com>
2015-11-25 11:13:52 +0100

    added mobile and synthese

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.listen(3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/', function(request, response){
-	response.send('{"name" : "Devoxx"}');
+	response.send('{"name" : "Codeurs en Seine"}');
 })
